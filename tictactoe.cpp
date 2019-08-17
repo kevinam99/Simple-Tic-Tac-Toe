bed12668e6b1c4515648a79f769d40435bbdc259
moriarty128 <gaurangsg.28@gmail.com>
2019-08-17 15:33:50 +0530

    fixed-retry-loop

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -35,50 +35,51 @@ int main()
 		   
 	}
 	system("cls");
-while(retry=='y)
-      {
-	do
+	while(retry=='y')
 	{
-		board();
-		player=(player%2)?1:2;
-		cout << "                                             Player " << player << ", enter a number:  ";
-		cin >> choice;
-		action=(player == 1) ? 'X' : 'O';
-		if (choice == 1 && square[1] == '1')
-			square[1] = action;
-		else if (choice == 2 && square[2] == '2')
-			square[2] = action;
-		else if (choice == 3 && square[3] == '3')
-			square[3] = action;
-		else if (choice == 4 && square[4] == '4')
-			square[4] = action;
-		else if (choice == 5 && square[5] == '5')
-			square[5] = action;
-		else if (choice == 6 && square[6] == '6')
-			square[6] = action;
-		else if (choice == 7 && square[7] == '7')
-			square[7] = action;
-		else if (choice == 8 && square[8] == '8')
-			square[8] = action;
-		else if (choice == 9 && square[9] == '9')
-			square[9] = action;
-		else
+		resetBoard();
+		do
 		{
-			cout<<"Invalid move. Try again.";
-			player--;
-			getch();
-		}
-		i=whoWon();
-		player++;
-	}while(i==-1);
-	board();
-	if(i==1)
-		cout<<"                                                    Player "<<--player<<" wins ";
-	else
-		cout<<"                                              Draw";
-	cout<<endl<<"                                   Would you like to try again? Enter y or n: ";
-	cin>>retry;
-      }
+			board();
+			player=(player%2)?1:2;
+			cout << "                                             Player " << player << ", enter a number:  ";
+			cin >> choice;
+			action=(player == 1) ? 'X' : 'O';
+			if (choice == 1 && square[1] == '1')
+				square[1] = action;
+			else if (choice == 2 && square[2] == '2')
+				square[2] = action;
+			else if (choice == 3 && square[3] == '3')
+				square[3] = action;
+			else if (choice == 4 && square[4] == '4')
+				square[4] = action;
+			else if (choice == 5 && square[5] == '5')
+				square[5] = action;
+			else if (choice == 6 && square[6] == '6')
+				square[6] = action;
+			else if (choice == 7 && square[7] == '7')
+				square[7] = action;
+			else if (choice == 8 && square[8] == '8')
+				square[8] = action;
+			else if (choice == 9 && square[9] == '9')
+				square[9] = action;
+			else
+			{
+				cout<<"Invalid move. Try again.";
+				player--;
+				getch();
+			}
+			i=whoWon();
+			player++;
+		}while(i==-1);
+		board();
+		if(i==1)
+			cout<<"                                                    Player "<<--player<<" wins ";
+		else
+			cout<<"                                              Draw";
+		cout<<endl<<"                                   Would you like to try again? Enter y or n: ";
+		cin>>retry;
+    }
 	return 0;
 }
 /*********************************************
